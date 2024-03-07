3a0f9e0962aa62760f64fd07a8e3e3d451d3984f
selusan <santoslsergio@gmail.com>
2024-03-07 10:23:23 -0300

    instalaçao gunicorn, e criaçao da função saudacao(ola_nome)

--- first_api.py
+++ first_api.py
@@ -2,8 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Flask'
+@app.get('/')
+def produto():
+    return f'Hello world !'
+
+@app.get('/nome/<ola_nome>')
+def saudacao(ola_nome):
+    return f'Olá {ola_nome}'
 
 
+#instalar o servidor wsgi com pip install
+if __name__ == '__main__':
+    app.run()
